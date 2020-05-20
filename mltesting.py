058106d9fc847cb8b2ed72dc4519b71408f9bae0
MassiveAtoms <s.ramsaransing.w@gmail.com>
2020-05-19 21:04:14 -0300

    cleaned up and documented some stuff

--- mltesting.py
+++ mltesting.py
@@ -14,23 +14,26 @@ with open("PET_paramaribo.json") as petfile:
 days = array(range(365))
 
 
-# # quick show me
+
+# simulate the values you'd be measuring. Say we have a pan with pan factor of 3.3
+# the higher the std is , the worse the predictions become and the more datapoints we need to predict accurately
+normal_transform = random.normal(3.3, 0.5, 365)  # 0.5 is the standard deviation. 
+pan_et = normal_transform * ET # values you'd be measuring.
+
+
+# # quick show me the distribution of the normal transform
 # sns.distplot(normal_transform)
 # plt.show()
 
-normal_transform = random.normal(3.3, 0.5, 365) 
-pan_et = normal_transform * ET
-# sns.lineplot(days, pan_et)
-# sns.lineplot(days, ET)
-# plt.show()
-lim = 10
+
+lim = 10 # days measured, ammount of data points
 train_ET = ET[:lim]
 train_pan = pan_et[:lim]
 model = sm.OLS(train_ET, train_pan).fit()
 res = model.predict(pan_et)
 print(model.summary())
 
-## very dumb regression, stops working better than OLS when the equation is ax + b
+## very naive "regression", stops working better than OLS when the equation is ax + b
 coefficient = mean(train_pan / train_ET)
 res1 = pan_et / coefficient
 print("Dumb implementation", coefficient)
@@ -38,6 +41,6 @@ print("Dumb implementation", coefficient)
 sns.lineplot(days, pan_et, label="measured(pan)")
 sns.lineplot(days, ET, label="FAO")
 sns.lineplot(days, res, label="predicted ET(Ordinary Least Squares)")
-sns.lineplot(days, res1, label="predicted ET(own 'regrassion')")
+# sns.lineplot(days, res1, label="predicted ET(own 'regrassion')") # removed for clarity's sake
 plt.ylim([0,20])
 plt.show()
