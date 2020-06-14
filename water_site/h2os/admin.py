d5ad16d3228e4f0f0064a0324a36ccc815d11dfa
Joshua Kartokario <you@example.com>
2020-06-14 11:45:55 -0300

    added base models , site admin and display

--- water_site/h2os/admin.py
+++ water_site/h2os/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Plant_group
 # Register your models here.
+
+admin.site.register(Plant_group)
--- water_site/h2os/models.py
+++ water_site/h2os/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Plant_group(models.Model):
+    loc_id = models.CharField(max_length = 2)
+    plant_name = models.CharField(max_length = 50)
+    growth_day = models.CharField(max_length = 4) # example 1.12 means first growth phase on the 2th day
+    distance = models.FloatField()
+    water_flowrate = models.FloatField()
+    last_irrigated = models.DateTimeField()
+    #add in a picture later
+
+
+def __str__(self):
+    return self.loc_id
+
